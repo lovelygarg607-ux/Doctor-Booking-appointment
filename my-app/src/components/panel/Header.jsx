ff4212c58f17f575825d47bf6bf07e989299cd39
lovelygarg607-ux <lovelygarg607@gmail.com>
2026-02-24 15:45:55 +0530

    Update Header.jsx

--- my-app/src/components/panel/Header.jsx
+++ my-app/src/components/panel/Header.jsx
@@ -14,6 +14,12 @@ const Header = () => {
 
       <header class="main-header">
             <div class="header-left">
+                  <button
+                    className="menu-btn"
+                    onClick={() => setIsOpen(true)}
+                >
+                    ☰
+                </button>
                 <div class="search-bar">
                     <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none"
                         stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
