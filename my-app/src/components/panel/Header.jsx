34bd10b4c246564386ec72eb63b4060d1b56d117
lovelygarg607-ux <lovelygarg607@gmail.com>
2026-02-24 15:58:10 +0530

    Update Header.jsx

--- my-app/src/components/panel/Header.jsx
+++ my-app/src/components/panel/Header.jsx
@@ -1,49 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./../../styles/dashboard.css"
 import { useSelector } from 'react-redux'
 
-
-
 const Header = () => {
-    const profileData = useSelector((state)=>  state.doctorprofile)
-    console.log(profileData)
-  return (
-   <>
-   
-   
+  const [isOpen, setIsOpen] = useState(false)
+  const profileData = useSelector((state)=> state.doctorprofile)
 
-      <header class="main-header">
-            <div class="header-left">
-                  <button
-                    className="menu-btn"
-                    onClick={() => setIsOpen(true)}
-                >
-                    ☰
-                </button>
-                <div class="search-bar">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none"
-                        stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-                        <circle cx="11" cy="11" r="8"></circle>
-                        <line x1="21" y1="21" x2="16.65" y2="16.65"></line>
-                    </svg>
-                    <input type="text" placeholder="Search..."/>
-                </div>
-            </div>
-            
-                <div class="header-profile">
-              {profileData?.profileImage?  (<img src={profileData.profileImage} alt="Admin-phto"/> ):<img src="https://i.pravatar.cc/150?u=admin" alt="Admin Photo"/>}
-                    <div class="header-profile-info">
-                   {profileData?.userID?.email? (<p>{profileData?.userID?.email}</p>)  :  (  <p>admin98@gmail.com </p>)}
-                      <span>Admin</span>
-                    </div>
-                </div>
-            
-        </header>
-   
-   
-   </>
+  return (
+    <header className="main-header">
+      <div className="header-left">
+        <button
+          className="menu-btn"
+          onClick={() => setIsOpen(true)}
+        >
+          ☰
+        </button>
+
+        <div className="search-bar">
+          <input type="text" placeholder="Search..." />
+        </div>
+      </div>
+
+      <div className="header-profile">
+        <img 
+          src={profileData?.profileImage || "https://i.pravatar.cc/150?u=admin"} 
+          alt="Admin" 
+        />
+      </div>
+    </header>
   )
 }
 
-
-export default Header;
+export default Header
